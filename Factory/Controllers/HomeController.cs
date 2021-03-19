ee84614d9ab4cfc2dad27abe6fa49797f9107465
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-19 13:32:56 -0700

    turn engineers and machines listed on home page into links that display their details

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
-// using System.Collections.Generic;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using System.Linq;
 
 namespace Factory.Controllers
 {
@@ -14,15 +10,7 @@ namespace Factory.Controllers
     {
       _db = db;
     }
-    // [HttpGet("/")]
-    // public ActionResult Index()
-    // {
-    //   var thisJoin = _db.EngineerMachine
-    //   .Include(engineerMachine => engineerMachine.JoinEntities)
-    //   .ThenInclude(join => join.Engineer)
-    //   .ThenInclude(join => join.Machine);
-    //   return View(thisJoin);
-    // }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
